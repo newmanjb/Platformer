f7dd10524489728395d9103cfdc010977c018c20
Joshua Newman <joshuajmnewman@gmail.com>
2018-07-05 16:04:16 +0100

    Create Constants.java
    Refactoring in order to introduce new types of object (Platform, JSW, Nasty etc..)

---
+++ PlatformScreenBuilder/src/main/java/com/noomtech/platformscreen/Constants.java
@@ -0,0 +1,11 @@
+package com.noomtech.platformscreen;
+
+public class Constants {
+
+
+    public static final String TYPE_JSW = "jsw";
+    public static final String TYPE_PLATFORM = "platform";
+    public static final String TYPE_NASTY = "nasty";
+    public static final String[] TYPES = {TYPE_PLATFORM, TYPE_NASTY, TYPE_JSW};
+
+}
