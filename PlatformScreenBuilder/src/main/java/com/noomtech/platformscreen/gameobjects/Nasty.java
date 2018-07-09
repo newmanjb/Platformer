f7de753129879c14cfba55a0efa5dd5668f29f3e
Joshua Newman <joshuajmnewman@gmail.com>
2018-07-09 12:56:53 +0100

    Update Nasty.java
    Added some functionality that enables the object to be set back to its default state

--- PlatformScreenBuilder/src/main/java/com/noomtech/platformscreen/gameobjects/Nasty.java
+++ PlatformScreenBuilder/src/main/java/com/noomtech/platformscreen/gameobjects/Nasty.java
@@ -10,6 +10,10 @@ import java.awt.*;
 public class Nasty extends GameObject {
 
 
+    //The direction along the y-axis that this nasty is moving.  Start the nasty off moving down
+    private int moveYDirection = 1;
+
+
     public Nasty(Rectangle collisionArea) {
         super(collisionArea);
     }
@@ -18,6 +22,33 @@ public class Nasty extends GameObject {
     public void paintIt(Graphics graphics) {
         graphics.setColor(Color.RED);
         Rectangle r = getCollisionArea();
-        graphics.drawRect(r.x, r.y, r.width, r.height);
+        graphics.fillRect(r.x, r.y, r.width, r.height);
+    }
+
+
+    public void setLocation(int x, int y) {
+        Rectangle r = getCollisionArea();
+        r.x = x;
+        r.y = y;
+    }
+
+    /**
+     * @return Which direction this nasty is moving in - 1 for down, -1 for up.
+     */
+    public int getMoveYDirection() {
+        return moveYDirection;
+    }
+
+    /**
+     * Gives this nasty the opportunity to update itself in reaction to a collision e.g. it might change direction
+     */
+    public void onCollision() {
+        //Go from moving down to up or from moving up to down
+        moveYDirection = -moveYDirection;
+    }
+
+    public void setBackToStartingState() {
+        super.setBackToStartingState();
+        moveYDirection = 1;
     }
 }
