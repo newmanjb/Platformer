6a710c4ba7793ad0d5ee454ecc00eb59a415e150
Joshua Newman <joshuajmnewman@gmail.com>
2018-07-13 12:54:04 +0100

    Update Constants.java
    Added lethal objects and finishing objects

--- PlatformScreenBuilder/src/main/java/com/noomtech/platformscreen/Constants.java
+++ PlatformScreenBuilder/src/main/java/com/noomtech/platformscreen/Constants.java
@@ -6,6 +6,8 @@ public class Constants {
     public static final String TYPE_JSW = "jsw";
     public static final String TYPE_PLATFORM = "platform";
     public static final String TYPE_NASTY = "nasty";
-    public static final String[] TYPES = {TYPE_PLATFORM, TYPE_NASTY, TYPE_JSW};
+    public static final String TYPE_LETHAL_OBJECT = "lethalObject";
+    public static final String TYPE_FINISHING_OBJECT = "finishingObject";
+    public static final String[] TYPES = {TYPE_PLATFORM, TYPE_NASTY, TYPE_JSW, TYPE_LETHAL_OBJECT, TYPE_FINISHING_OBJECT};
 
 }
