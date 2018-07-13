596e26b741dd298148a0b4fa459a3669165d2692
Joshua Newman <joshuajmnewman@gmail.com>
2018-07-13 12:55:47 +0100

    Add files via upload
    Renamed from MovementType

---
+++ PlatformScreenBuilder/src/main/java/com/noomtech/platformscreen/movement/PlayerMovementType.java
@@ -0,0 +1,11 @@
+package com.noomtech.platformscreen.movement;
+
+
+public enum PlayerMovementType {
+    WALK_LEFT,
+    WALK_RIGHT,
+    JUMP,
+    FALL,
+    //e.g. For when the player dies
+    MOVED_BACK_TO_START;
+}
