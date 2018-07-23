ac65196f220efdf12c19fee5a07a23b91e397317
Joshua Newman <joshuajmnewman@gmail.com>
2018-07-23 11:48:45 +0100

    Add files via upload
    test

---
+++ Game.java
@@ -0,0 +1,16 @@
+package com.noomtech.platformscreen;
+
+import com.noomtech.platformscreen.frame.GameFrame;
+
+
+/**
+ * Main entry point.
+ * @author Joshua Newman
+ */
+public class Game {
+
+
+    public static void main(String[] args) throws Exception {
+        new GameFrame();
+    }
+}
