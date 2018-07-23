f7d6ccec6c37e668ce4b0e96e18a9ef7ab91210b
Joshua Newman <joshuajmnewman@gmail.com>
2018-07-23 11:57:22 +0100

    Add files via upload
    Main method now throws an exception

--- PlatformScreenBuilder/src/main/java/com/noomtech/platformscreenbuilder/Main.java
+++ PlatformScreenBuilder/src/main/java/com/noomtech/platformscreenbuilder/Main.java
@@ -5,7 +5,7 @@ import com.noomtech.platformscreenbuilder.frame.MainFrame;
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         new MainFrame();
     }
 }
