58deb5ccb8a7fac5e4acba51f66228628289746a
Joshua Newman <joshuajmnewman@gmail.com>
2018-09-03 14:45:18 +0100

    Update GameFrame.java
    Game object types now stored as class names so as they can be instantiated using reflection

--- PlatformScreenBuilder/src/main/java/com/noomtech/platformscreen/frame/GameFrame.java
+++ PlatformScreenBuilder/src/main/java/com/noomtech/platformscreen/frame/GameFrame.java
@@ -1,13 +1,11 @@
 package com.noomtech.platformscreen.frame;
 
 import com.datastax.driver.core.*;
-import com.noomtech.platformscreen.Constants;
 import com.noomtech.platformscreen.gameobjects.*;
 import com.noomtech.platformscreen.gamethread.GamePanel;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 import java.util.*;
 import java.util.List;
 
@@ -23,19 +21,15 @@ public class GameFrame extends JFrame {
     private final Cluster cluster;
 
     //@todo - save the screen size in the editor, don't hardcode it
-    public GameFrame() throws IOException {
+    public GameFrame() throws Exception {
 
         Cluster.Builder b = Cluster.builder().addContactPoint("10.130.84.52");
         cluster = b.build();
         cassandraSession = cluster.connect();
 
-        Map<String, List<? extends GameObject>> gameObjects = load();
+        List<GameObject> gameObjects = load();
         gamePanel = new GamePanel(
-                (List<JSW>)gameObjects.get(Constants.TYPE_JSW),
-                (List<Platform>)gameObjects.get(Constants.TYPE_PLATFORM),
-                (List<Nasty>)gameObjects.get(Constants.TYPE_NASTY),
-                (List<StaticLethalObject>)gameObjects.get(Constants.TYPE_LETHAL_OBJECT),
-                (List<FinishingObject>)gameObjects.get(Constants.TYPE_FINISHING_OBJECT));
+                gameObjects);
 
         setTitle("Game");
 
@@ -80,19 +74,15 @@ public class GameFrame extends JFrame {
     }
 
     /**
-     * Loads the game data from the cassandra db
+     * Loads the game data from the cassandra db.
      */
-    private Map<String,List<? extends GameObject>> load() throws IOException {
+    private List<GameObject> load() throws Exception {
+
         String selectStatement = "SELECT class,id,attributes,rectangle FROM jsw.COLLISION_AREAS";
         BoundStatement b = cassandraSession.prepare(selectStatement).bind();
         ResultSet rs = cassandraSession.execute(b);
-        Map<String,List<? extends GameObject>> toReturn = new HashMap<>();
-        List<Nasty> nasties = new ArrayList<>();
-        List<JSW> player = new ArrayList<>();
-        List<Platform> platforms = new ArrayList<>();
-        List<StaticLethalObject> staticLethalObjects = new ArrayList<>();
-        List<FinishingObject> finishingObjects = new ArrayList<>();
         List<Row> rows = rs.all();
+        List<GameObject> toReturn = new ArrayList<>();
         for(Row row : rows) {
 
             TupleValue tupleValue = row.getTupleValue(3);
@@ -101,45 +91,14 @@ public class GameFrame extends JFrame {
             TupleValue endTuple = tupleValue.get(1,TupleValue.class);
             Point end = new Point(endTuple.get(0,Integer.class), endTuple.get(1,Integer.class));
             Rectangle rectangle = new Rectangle(start.x, start.y, end.x - start.x, end.y - start.y);
-
             String classVal = row.getString(0);
             Map<String,String> attributes = row.getMap(2, String.class, String.class);
-            switch(classVal) {
-                case(Constants.TYPE_JSW) : {
-                    JSW jsw = new JSW(rectangle);
-                    player.add(jsw);
-                    break;
-                }
-                case(Constants.TYPE_NASTY) : {
-                    Nasty nasty = new Nasty(rectangle);
-                    nasties.add(nasty);
-                    break;
-                }
-                case(Constants.TYPE_PLATFORM): {
-                    Platform platform = new Platform(rectangle);
-                    platforms.add(platform);
-                    break;
-                }
-                case(Constants.TYPE_LETHAL_OBJECT): {
-                    StaticLethalObject staticLethalObject = new StaticLethalObject(rectangle);
-                    staticLethalObjects.add(staticLethalObject);
-                    break;
-                }
-                case(Constants.TYPE_FINISHING_OBJECT): {
-                    FinishingObject finishingObject = new FinishingObject(rectangle);
-                    finishingObjects.add(finishingObject);
-                    break;
-                }
-                default: {
-                    throw new IllegalArgumentException("Unknown class " + classVal);
-                }
-            }
+            //The type of game object is represented as the full name of the underlying class so as it can be instantiated
+            //using reflection
+            GameObject gameObject = (GameObject)Class.forName(classVal).getConstructor(Rectangle.class).newInstance(rectangle);
+            gameObject.setAttributes(attributes);
+            toReturn.add(gameObject);
         }
-        toReturn.put(Constants.TYPE_JSW, player);
-        toReturn.put(Constants.TYPE_PLATFORM, platforms);
-        toReturn.put(Constants.TYPE_NASTY, nasties);
-        toReturn.put(Constants.TYPE_LETHAL_OBJECT, staticLethalObjects);
-        toReturn.put(Constants.TYPE_FINISHING_OBJECT, finishingObjects);
 
         return toReturn;
     }
