9f39ff5d97e5de293bd0b98cac01c71179d295cc
Joshua Newman <joshuajmnewman@gmail.com>
2018-10-01 10:01:06 +0100

    Add files via upload
    1: Renamed getArea method to getImageArea
    2: Moved setLocation method up into GameObject

--- PlatformScreenBuilder/src/main/java/com/noomtech/jsw/game/gameobjects/GameObject.java
+++ PlatformScreenBuilder/src/main/java/com/noomtech/jsw/game/gameobjects/GameObject.java
@@ -5,14 +5,14 @@ import java.util.Map;
 
 
 /**
- * Represents an object on the screen e.g. player, platform
+ * Represents an object on the screen e.g. player, platform.
  * @author Joshua Newman
  */
 public abstract class GameObject {
 
 
     /** The boundaries of the object's image.  This is used to define its location **/
-    private Rectangle area;
+    private Rectangle imageArea;
     //The properties of the object
     private Map<String,String> attributes;
     //This is where the game object is first located when the game starts
@@ -20,19 +20,11 @@ public abstract class GameObject {
     //-- Reinstate if needed --
     //Set to true once this object has been drawn.  Used by objects that never change in appearance during the game
     //protected boolean staticObjectsDontNeedToBeDrawnAgain;
-    /**
-     * These define the areas into which other objects have to move into to be considered as "touching" the object.  This may
-     * not necessarily be the same as the "area" field, as it depends on the shape of the image that represents the
-     * object.  There could be several of these on one object if, for example, the image for the object is a particularly
-     * irregular shape as opposed to being squarish.
-     */
-    protected Rectangle[] collisionAreas;
 
 
     public GameObject(Rectangle area, Map<String,String> attributes) {
-        this.area = area;
-        //Default the collision area to be the area occupied by the entire image
-        this.collisionAreas = new Rectangle[]{this.area};
+        this.imageArea = area;
+
         this.attributes = attributes;
         startingLocation = area.getLocation();
     }
@@ -48,20 +40,12 @@ public abstract class GameObject {
         //staticObjectsDontNeedToBeDrawnAgain = CommonUtils.gameIsRunning;
     }
 
-    public Rectangle getArea() {
-        return area;
+    public Rectangle getImageArea() {
+        return imageArea;
     }
 
-    public Rectangle[] getCollisionAreas() {
-        return collisionAreas;
-    }
-
-    public void setCollisionAreas(Rectangle[] collisionAreas) {
-        this.collisionAreas = collisionAreas;
-    }
-
-    public void setArea(Rectangle area) {
-        this.area = area;
+    public void setImageArea(Rectangle imageArea) {
+        this.imageArea = imageArea;
     }
 
     public Map<String,String> getAttributes() {
@@ -69,22 +53,28 @@ public abstract class GameObject {
     }
 
     public int getHeight() {
-        return area.height;
+        return imageArea.height;
     }
 
     public int getWidth() {
-        return area.width;
+        return imageArea.width;
     }
 
     public int getX() {
-        return area.x;
+        return imageArea.x;
     }
 
     public int getY() {
-        return area.y;
+        return imageArea.y;
+    }
+
+    public void setLocation(int x, int y) {
+        Rectangle r = getImageArea();
+        r.x = x;
+        r.y = y;
     }
 
     public Point getLocation() {
-        return area.getLocation();
+        return imageArea.getLocation();
     }
 }
