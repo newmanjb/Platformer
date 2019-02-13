a5373357c4480a35ba2272cdb0ef314721963c4b
joshuancrypt <joshua@ncrypt.com>
2019-02-13 10:48:11 +0000

    Renamed a few classes to make things clearer

--- PlatformScreenBuilder/src/main/java/com/noomtech/jsw/game/movement/ComputerControlledObject.java
+++ PlatformScreenBuilder/src/main/java/com/noomtech/jsw/game/movement/ComputerControlledObject.java
@@ -5,7 +5,7 @@ package com.noomtech.jsw.game.movement;
  * Represents an object that dictates its own movement, meaning that it's controlled by the game application as
  * opposed to something like the {@link com.noomtech.jsw.game.gameobjects.objects.JSW} which is controlled by the user
  */
-public interface SelfControlledMovingObject {
+public interface ComputerControlledObject {
     /**
      * Called by the application logic.  Move the object and return true if the move has hit the player
      */
